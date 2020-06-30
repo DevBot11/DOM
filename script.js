b140af383da67c61a5b5b18bcbf41d3ef009f84e
Sean <lynpalo09@gmail.com>
2020-06-30 19:43:02 -0400

    variable update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
+	let li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
